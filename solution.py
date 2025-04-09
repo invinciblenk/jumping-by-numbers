7b0420c76434751c7904f4e07660abc3baf8e765
Nikita Kordonets <83768585+invinciblenk@users.noreply.github.com>
2025-04-09 21:45:17 +0300

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,26 @@
+def jump(nums):
+    num_element = len(nums)
+    i = 0
+    visited = set()  #отслеживаем посещенные индексы чтобы в будущем бороться с зацикливанием вроде [3 -1 2 -2 6]
+    while i < num_element:
+        if nums[i] == 0: #с нулем мы далеко не уйдём...
+            return False
+        else:
+            i = i + nums[i]
+        if i in visited:  # Проверка на зацикливание
+            return False
+        visited.add(i)
+        if i < 0: #учитываем вылет в отрицательные i :)
+            return False
+    return True
+
+
+user_input = input('Введите элементы массива, раделенные пробелами: ')
+
+array = user_input.split()
+
+array = [int(x) for x in array]
+
+print('Ваш массив: ', array)
+print(jump(array))
+#invincible_nk :)
